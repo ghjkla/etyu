2985911c6e9bfff9cb96c45b71b58e9d8a3e9a4b
ghjkla <31461783+ghjkla@users.noreply.github.com>
2017-11-17 15:29:25 +0900

    Add files via upload

---
+++ electronicMaze/electronicMaze.ino
@@ -0,0 +1,27 @@
+int red = 11;
+int buzzer = 13;
+int sensor = 2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT); 
+  pinMode(red, OUTPUT);
+  pinMode(sensor, INPUT_PULLUP);
+}
+
+void loop() {
+  int sensorVal = digitalRead(sensor);
+  Serial.println(sensorVal);
+
+  if (sensorVal == LOW) {
+      digitalWrite(buzzer, HIGH);
+    digitalWrite(red, HIGH); 
+    delay(1000);
+ 
+
+  } else {
+        digitalWrite(buzzer, LOW);
+    digitalWrite(red, LOW); 
+ 
+  }
+}
